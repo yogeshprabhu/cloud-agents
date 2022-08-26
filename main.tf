ff9e8a0aa9766a3bc660b2e961a22fe4b48c928d
Yogesh <yogesh.prabhu@gmail.com>
2022-08-26 11:27:37 -0400

    initial file
    initial files

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx
+  name  = "nginx"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
